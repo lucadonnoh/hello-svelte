9afe10c3b14193dd10dd0d3f3b61905b0024b6f5
lucadonnoh <donnoh99@gmail.com>
2020-12-29 16:10:00 +0100

    mouse position

--- src/App.svelte
+++ src/App.svelte
@@ -11,8 +11,7 @@
 
 	let logStress = 10;
 
-	function incLogCount()
-	{
+	function incLogCount() {
 		logCount += 1;
 	}
 
@@ -21,23 +20,23 @@
 	}
 
 	// reactivity is only triggered by assignements, so .push doesn't cause an update
-	$: if (count>=0) { 
+	$: if (count >= 0) {
 		console.log("the count is high af");
 	}
 
-	let user = { loggedIn: false }
+	let user = { loggedIn: false };
 
 	let dataLeak = "lol now i have all your data";
 
 	function toggle() {
-		if(logCount <= logStress*2) {
+		if (logCount <= logStress * 2) {
 			user.loggedIn = !user.loggedIn;
 		} else {
 			dataLeak = "ok i'm stopping this too";
 		}
 
-		if(logCount < logStress) {
-			logState = (user.loggedIn) ? "Log Out" : "Log In";
+		if (logCount < logStress) {
+			logState = user.loggedIn ? "Log Out" : "Log In";
 		} else {
 			logState = "for god's sake stop ping pongin this button";
 		}
@@ -46,10 +45,16 @@
 	}
 
 	let logState = "Log In";
+
+	let m = { x: 0, y: 0 };
+
+	function handleMousemove(event) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
 </script>
 
 <style>
-
 	main {
 		display: flex;
 		flex-direction: column;
@@ -58,17 +63,28 @@
 	}
 	h1 {
 		text-align: center;
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+			Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 		font-size: 2em;
+	}
+
+	.mouse {
+		height: 100%;
+		width: 100%;
+		text-align: center;
 	}
 </style>
 
-<main>
+<main on:mousemove={handleMousemove}>
 	<h1>Hello {name.toUpperCase()}</h1>
-	<Nested curse="<strong>fuckin</strong>"/>
+	<Nested curse="<strong>fuckin</strong>" />
+
+	<p class="mouse">The mouse position is {m.x} x {m.y}</p>
 
 	<button on:click={handleClick}>
-		Clicked {count} {count === 1 ? 'time' : 'times'}
+		Clicked
+		{count}
+		{count === 1 ? 'time' : 'times'}
 	</button>
 
 	<p>and doubled is {doubled}</p>
@@ -78,14 +94,11 @@
 			{logState}
 		</button>
 		<p>{dataLeak}</p>
-	{:else}
-		<button on:click={toggle}>
-			{logState}
-		</button>
-	{/if}
+	{:else}<button on:click={toggle}> {logState} </button>{/if}
 
 	{#if logCount >= logStress * 3}
 		<p>what about now</p>
-		<FunnyPeople/>
+		<FunnyPeople />
 	{/if}
+
 </main>
